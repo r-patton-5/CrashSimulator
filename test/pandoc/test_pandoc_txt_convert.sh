2d6b067992ef038c3509c5a2cc7c69299317360d
Ryan <rjp2@williams.edu>
2017-06-29 17:49:05 -0400

    return pandoc test to clean state

--- test/pandoc/test_pandoc_txt_convert.sh
+++ test/pandoc/test_pandoc_txt_convert.sh
@@ -6,9 +6,8 @@ strace -f -s 9999 -vvvv -o pandoc_convert_txt.strace pandoc -t plain -o converte
 rm converted.txt;
 
 cd ../../ > /dev/null;
-python main.py -c "['pandoc', '-t', 'plain', '-o', 'converted.txt', 'empty.txt']" -t test/pandoc/pandoc_convert_txt.strace;# -l DEBUG;
+python main.py -c "['pandoc', '-t', 'plain', '-o', 'converted.txt', 'empty.txt']" -t test/pandoc/pandoc_convert_txt.strace;
 
 cd test/pandoc/;
 rm empty.txt;
-#rm converted.txt;
-#rm pandoc_convert_txt.strace
+rm pandoc_convert_txt.strace
