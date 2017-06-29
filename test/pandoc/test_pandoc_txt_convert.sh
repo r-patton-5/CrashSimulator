d1e015e1d7a94cd4424c1c5a127aa68fc4cfc7ec
Ryan <rjp2@williams.edu>
2017-06-29 17:41:07 -0400

    changed pandoc test back to original (moving files revelaed nothing)

--- test/pandoc/test_pandoc_txt_convert.sh
+++ test/pandoc/test_pandoc_txt_convert.sh
@@ -2,13 +2,13 @@
 
 touch empty.txt;
 
-#strace -f -s 9999 -vvvv -o pandoc_convert_txt.strace pandoc -t latex -o ../converted.txt empty.txt;
-#rm ../converted.txt;
+strace -f -s 9999 -vvvv -o pandoc_convert_txt.strace pandoc -t latex -o converted.txt empty.txt;
+rm converted.txt;
 
 cd ../../ > /dev/null;
-python main.py -c "['pandoc', '-t', 'latex', '-o', '../converted.txt', 'empty.txt']" -t test/pandoc/pandoc_convert_txt.strace;# -l DEBUG;
+python main.py -c "['pandoc', '-t', 'latex', '-o', 'converted.txt', 'empty.txt']" -t test/pandoc/pandoc_convert_txt.strace;# -l DEBUG;
 
 cd test/pandoc/;
 rm empty.txt;
-#rm ../converted.txt;
+#rm converted.txt;
 #rm pandoc_convert_txt.strace
